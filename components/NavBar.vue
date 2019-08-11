ce0239e98931852f7dce74f53e0c68d9f17488eb
KAWAKUBO Toru <other@kwkbtr.info>
2019-08-11 17:40:12 +0900

    Fix navigation links

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,8 +2,8 @@
   <nav>
     <ul>
       <nuxt-link to="/">数量管理</nuxt-link>
-      <nuxt-link to="add-item">項目追加</nuxt-link>
-      <nuxt-link to="add-category">カテゴリー追加</nuxt-link>
+      <nuxt-link to="/add-item">項目追加</nuxt-link>
+      <nuxt-link to="/add-category">カテゴリー追加</nuxt-link>
     </ul>
   </nav>
 </template>
